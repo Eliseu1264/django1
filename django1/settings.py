f1a761c76e3a108f8657c1ed110b384bb699f940
Eliseu Bento <eliseu1264@gmail.com>
2022-09-20 14:11:19 -0300

    cmt 45

--- django1/settings.py
+++ django1/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whidenoise.middleware.WhideNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
