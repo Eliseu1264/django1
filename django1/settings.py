da0f0341eff6e5b47efd8d0f4dd28ef3d98059ce
Eliseu Bento <eliseu1264@gmail.com>
2022-09-23 06:43:08 -0300

    commit - 23-09 - 3

--- django1/settings.py
+++ django1/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/' # usado em desenvolvimento
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # usado em produção
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # usado em produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
