ecee5b28b2de2f46173bac5b330a8bd7a1fdb503
Eliseu Bento <eliseu1264@gmail.com>
2022-09-23 06:54:12 -0300

    cfsafdsafasdf

--- django1/settings.py
+++ django1/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/' # usado em desenvolvimento
+STATIC_URL = './static/' # usado em desenvolvimento
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') # usado em produção
 
 # Default primary key field type
